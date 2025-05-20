e092476f0467df8cb5ea9ad0fdd7d8b2e774fbc6
Benedikt Bongartz <bongartz@klimlive.de>
2025-05-20 18:27:26 +0200

    apps: implement openAI chat completion endpoint
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>

--- clusters/homelab/apps/llm/llama-stack/agent/testing.sh
+++ clusters/homelab/apps/llm/llama-stack/agent/testing.sh
@@ -17,3 +17,25 @@ curl http://localhost:8000/v1/completions   -H "Content-Type: application/json" 
   }'
 
 # {"id":"cmpl-1234","object":"text_completion","created":1747749081,"model":"vllm","choices":[{"text":"[{\"name\": \"calculator\", \"arguments\": {\"x\": 40, \"y\": 30, \"operation\": \"add\"}}]","index":0,"logprobs":null,"finish_reason":"stop"}]}
+
+curl -X POST http://localhost:8000/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "vllm",
+    "messages": [
+      {"role": "user", "content": "Hello"}
+    ]
+  }'
+
+# {"id":"chatcmpl-1234","object":"chat.completion","created":1747758227,"model":"vllm","choices":[{"index":0,"message":{"role":"assistant","content":"Hello! How can I assist you today?"},"finish_reason":"stop"}]}
+
+curl -X POST http://localhost:8000/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "vllm",
+    "messages": [
+      {"role": "user", "content": "What is 40+30?"}
+    ]
+  }'
+
+# {"id":"chatcmpl-1234","object":"chat.completion","created":1747758297,"model":"vllm","choices":[{"index":0,"message":{"role":"assistant","content":"[{\"name\": \"calculator\", \"arguments\": {\"x\": 40, \"y\": 30, \"operation\": \"add\"}}]"}
